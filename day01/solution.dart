4e3a31a3cdb28f2e3096d691c914e3d7b779bb2e
Fredrik Wendel <fredrik.wendel@cygni.se>
2022-12-01 22:28:48 +0100

    chore(day01): rename test file

--- day01/solution.dart
+++ day01/solution.dart
@@ -3,8 +3,7 @@ import 'dart:io';
 List<int> elfCalories(String fileName) {
   var input = File.fromUri(Uri.file(fileName)).readAsLinesSync();
 
-  var elfs = <int>[0];
-  input.fold(elfs, (elfs, calories) {
+  var elfs = input.fold(<int>[0], (elfs, calories) {
     if (calories == "") {
       elfs.add(0);
     } else {
@@ -23,7 +22,5 @@ num part2({String fileName = "input.txt"}) {
   return elfs.sublist(elfs.length - 3).reduce((sum, curr) => sum += curr);
 }
 
-void main(List<String> arguments) {
-  exitCode = 0;
-  stdout.write((Platform.environment["part"] ?? "part1") == "part1" ? part1() : part2());
-}
+void main(List<String> arguments) =>
+    stdout.write((Platform.environment["part"] ?? "part1") == "part1" ? part1() : part2());
