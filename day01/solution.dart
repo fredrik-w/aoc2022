8637de8a50cbf47a16b60f161700ae06aa0b4214
Fredrik Wendel <fredrik.wendel@cygni.se>
2022-12-12 20:34:55 +0100

    chore(day01): reduce loc

--- day01/solution.dart
+++ day01/solution.dart
@@ -1,23 +1,17 @@
 import 'dart:io';
 
-List<int> elfCalories(String fileName) {
-  var input = File.fromUri(Uri.file(fileName)).readAsLinesSync();
+List<String> readFileAsLines(String fileName) => File.fromUri(Uri.file(fileName)).readAsLinesSync();
 
-  var elfs = input.fold(<int>[0], (elfs, calories) {
-    if (calories == "") {
-      return elfs..add(0);
-    }
-    elfs[elfs.length - 1] += int.parse(calories);
-    return elfs;
-  });
-  return elfs..sort((a, b) => a.compareTo(b));
-}
+List<int> elfCalories(List<String> lines) => lines.fold(<int>[0], (elfs, calories) {
+      if (calories == "") return elfs..add(0);
+      elfs.last += int.parse(calories);
+      return elfs;
+    })
+      ..sort((a, b) => a.compareTo(b));
 
-num part1({String fileName = "input.txt"}) => elfCalories(fileName).last;
+num part1({String fileName = "input.txt"}) => elfCalories(readFileAsLines(fileName)).last;
 
-num part2({String fileName = "input.txt"}) {
-  var elfs = elfCalories(fileName);
-  return elfs.sublist(elfs.length - 3).reduce((sum, curr) => sum += curr);
-}
+num part2({String fileName = "input.txt"}) =>
+    elfCalories(readFileAsLines(fileName)).reversed.take(3).reduce((sum, curr) => sum + curr);
 
 void main(List<String> arguments) => print((Platform.environment["part"] ?? "part1") == "part1" ? part1() : part2());
